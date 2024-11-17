fd0f061f4920a3c3830fc96bbae328983661a365
Łukasz Lachowski <writetozbyszko@gmail.com>
2024-11-17 10:32:59 +0100

    vcv-rack: build definition of `VCV Rack` now uses pinned git submodules of its repository for its dependencies instead of fetching each dependency separately

--- pkgs/by-name/vc/vcv-rack/package.nix
+++ pkgs/by-name/vc/vcv-rack/package.nix
@@ -1,12 +1,11 @@
 { alsa-lib
+, autoconf
+, automake
+, libtool
 , cmake
 , copyDesktopItems
 , curl
-, fetchFromBitbucket
-, fetchFromGitHub
-, ghc_filesystem
 , glew
-, glfw
 , zenity
 , gtk3-x11
 , imagemagick
@@ -21,97 +20,20 @@
 , makeDesktopItem
 , makeWrapper
 , pkg-config
-, rtmidi
-, speexdsp
 , stdenv
 , wrapGAppsHook3
 , zstd
 }:
 
 let
-  # The package repo vendors some of the package dependencies as submodules.
-  # Unfortunately, they are not pinned, so we have no guarantee that they
-  # will be stable, and therefore, we can't use them directly. Instead
-  # we'll have to fetch them separately ourselves.
-  pffft-source = fetchFromBitbucket {
-    owner = "jpommier";
-    repo = "pffft";
-    rev = "fbc4058602803f40dc554b8a5d2bcc694c005f2f";
-    sha256 = "16biji3115232cr1j975hpxw68lfybajlspnhfjcwg8jz2d8ybrf";
-  };
-  fuzzysearchdatabase-source = fetchFromBitbucket {
-    owner = "j_norberg";
-    repo = "fuzzysearchdatabase";
-    rev = "23122d1ff60d936fd766361a30210c954e0c5449";
-    sha256 = "1s88blx1rn2racmb8n5g0kh1ym7v21573l5m42c4nz266vmrvrvz";
-  };
-  nanovg-source = fetchFromGitHub {
-    owner = "VCVRack";
-    repo = "nanovg";
-    rev = "0bebdb314aff9cfa28fde4744bcb037a2b3fd756";
-    sha256 = "HmQhCE/zIKc3f+Zld229s5i5MWzRrBMF9gYrn8JVQzg=";
-  };
-  nanosvg-source = fetchFromGitHub {
-    owner = "memononen";
-    repo = "nanosvg";
-    rev = "9da543e8329fdd81b64eb48742d8ccb09377aed1";
-    sha256 = "1pkzv75kavkhrbdd2kvq755jyr0vamgrfr7lc33dq3ipkzmqvs2l";
-  };
-  osdialog-source = fetchFromGitHub {
-    owner = "AndrewBelt";
-    repo = "osdialog";
-    rev = "d0f64f0798c2e47f61d90a5505910ff2d63ca049";
-    sha256 = "1d3058x6wgzw7b0wai792flk7s6ffw0z4n9sl016v91yjwv7ds3a";
-  };
-  oui-blendish-source = fetchFromGitHub {
-    owner = "VCVRack";
-    repo = "oui-blendish";
-    rev = "2fc6405883f8451944ed080547d073c8f9f31898";
-    sha256 = "1bs0654312555vm7nzswsmky4l8759bjdk17pl22p49rw9k4a1px";
-  };
-  simde-source = fetchFromGitHub {
-    owner = "simd-everywhere";
-    repo = "simde";
-    rev = "416091ebdb9e901b29d026633e73167d6353a0b0";
-    sha256 = "064ygc6c737yjx04rydwwhkr4n4s4rbvj27swxwyzvp1h8nka6xf";
-  };
-  tinyexpr-source = fetchFromGitHub {
-    owner = "codeplea";
-    repo = "tinyexpr";
-    rev = "9907207e5def0fabdb60c443517b0d9e9d521393";
-    sha256 = "0xbpd09zvrk2ppm1qm1skk6p50mqr9mzjixv3s0biqq6jpabs88l";
-  };
-  fundamental-source = fetchFromGitHub {
-    owner = "VCVRack";
-    repo = "Fundamental";
+  fundamental-source = builtins.fetchGit {
+    url = "https://github.com/VCVRack/Fundamental.git";
     rev = "5ed79544161e0fa9a55faa7c0a5f299e828e12ab"; # tip of branch v2
-    sha256 = "0c6qpigyr0ppvra20hcy1fdcmqa212jckb9wkx4f6fgdby7565wv";
-  };
-  vcv-rtaudio = stdenv.mkDerivation rec {
-    pname = "vcv-rtaudio";
-    version = "unstable-2020-01-30";
-
-    src = fetchFromGitHub {
-      owner = "VCVRack";
-      repo = "rtaudio";
-      rev = "ece277bd839603648c80c8a5f145678e13bc23f3"; # tip of master branch
-      sha256 = "11gpl0ak757ilrq4fi0brj0chmlcr1hihc32yd7qza4fxjw2yx2v";
-    };
-
-    nativeBuildInputs = [ cmake pkg-config ];
-
-    buildInputs = [ alsa-lib libjack2 libpulseaudio ];
-
-    cmakeFlags = [
-      "-DRTAUDIO_API_ALSA=ON"
-      "-DRTAUDIO_API_PULSE=ON"
-      "-DRTAUDIO_API_JACK=ON"
-      "-DRTAUDIO_API_CORE=OFF"
-    ];
+    submodules = true;
   };
 in
 stdenv.mkDerivation rec {
-  pname = "vcv-rack";
+  pname = "VCV-Rack";
   version = "2.5.1";
 
   desktopItems = [
@@ -128,55 +50,18 @@ stdenv.mkDerivation rec {
     })
   ];
 
-  src = fetchFromGitHub {
-    owner = "VCVRack";
-    repo = "Rack";
-    rev = "v${version}";
-    sha256 = "1q2bwjfn6crk9lyd6m3py0v754arw1xgpv5kkj6ka1bc2yz839qh";
+  src = builtins.fetchGit {
+    url = "https://github.com/VCVRack/Rack.git";
+    ref = "refs/tags/v${version}";
+    rev = "3f133d8a0359b539bd262a4c3e1e6b4fb2ef83e6";
+    submodules = true;
   };
 
-  patches = [
-    ./rack-minimize-vendoring.patch
-  ];
-
-  prePatch = ''
-    # As we can't use `make dep` to set up the dependencies (as explained
-    # above), we do it here manually
-    mkdir -p dep/include
-
-    cp -r ${pffft-source}/* dep/pffft
-    cp -r ${fuzzysearchdatabase-source}/* dep/fuzzysearchdatabase
-    cp -r ${nanovg-source}/* dep/nanovg
-    cp -r ${nanosvg-source}/* dep/nanosvg
-    cp -r ${osdialog-source}/* dep/osdialog
-    cp -r ${oui-blendish-source}/* dep/oui-blendish
-    cp -r ${simde-source}/* dep/simde
-    cp -r ${tinyexpr-source}/* dep/tinyexpr
-
-    cp dep/pffft/*.h dep/include
-    cp dep/fuzzysearchdatabase/src/*.hpp dep/include
-    cp dep/nanosvg/**/*.h dep/include
-    cp dep/nanovg/src/*.h dep/include
-    cp dep/osdialog/*.h dep/include
-    cp dep/oui-blendish/*.h dep/include
-    cp -r dep/simde/simde dep/include
-    cp dep/tinyexpr/tinyexpr.h dep/include
-
-    # Build and dist the Fundamental plugins
-    cp -r ${fundamental-source} plugins/Fundamental/
-    chmod -R +rw plugins/Fundamental # will be used as build dir
-    substituteInPlace plugin.mk --replace ":= all" ":= dist"
-    substituteInPlace plugins/Fundamental/src/Logic.cpp \
-      --replace \
-        "LightButton<VCVBezelBig, VCVBezelLightBig<WhiteLight>>" \
-        "struct rack::componentlibrary::LightButton<VCVBezelBig, VCVBezelLightBig<WhiteLight>>"
-
-    # Fix reference to zenity
-    substituteInPlace dep/osdialog/osdialog_zenity.c \
-      --replace 'zenityBin[] = "zenity"' 'zenityBin[] = "${zenity}/bin/zenity"'
-  '';
-
   nativeBuildInputs = [
+    cmake
+    autoconf
+    automake
+    libtool
     copyDesktopItems
     imagemagick
     jq
@@ -185,12 +70,11 @@ stdenv.mkDerivation rec {
     pkg-config
     wrapGAppsHook3
   ];
+
   buildInputs = [
     alsa-lib
     curl
-    ghc_filesystem
     glew
-    glfw
     zenity
     gtk3-x11
     jansson
@@ -198,15 +82,28 @@ stdenv.mkDerivation rec {
     libjack2
     libpulseaudio
     libsamplerate
-    rtmidi
-    speexdsp
-    vcv-rtaudio
     zstd
   ];
+
+  prePatch = ''
+    # Build and dist the Fundamental plugins
+    cp -r ${fundamental-source} plugins/Fundamental/
+    chmod -R +rw plugins/Fundamental # will be used as build dir
+    substituteInPlace plugin.mk --replace ".DEFAULT_GOAL := all" ".DEFAULT_GOAL := dist"
+
+    # Fix reference to zenity
+    substituteInPlace dep/osdialog/osdialog_zenity.c \
+      --replace 'zenityBin[] = "zenity"' 'zenityBin[] = "${zenity}/bin/zenity"'
+  '';
+
+  patches = [ ./rack-minimize-vendoring.patch ];
+
+  configurePhase = '':'';
 
   makeFlags = lib.optionals (stdenv.buildPlatform != stdenv.hostPlatform) [
     "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
   ] ++ [
+    "dep"
     "all"
     "plugins"
   ];
@@ -236,6 +133,7 @@ stdenv.mkDerivation rec {
   '';
 
   dontWrapGApps = true;
+
   postFixup = ''
     # Wrap gApp and override the default global resource file directory
     wrapProgram $out/bin/Rack \
